6dea3043e5f41f4d2653ad06f3efd0486fdaace8
KhanhDuy <nguyenkhanhduy.29042004tv@gmail.com>
2025-12-06 14:24:52 +0700

    finish GET POST DELETE PUT for user

--- backend/backend/src/main/java/com/foodapp/backend/users/UserController.java
+++ backend/backend/src/main/java/com/foodapp/backend/users/UserController.java
@@ -1,6 +1,7 @@
 package com.foodapp.backend.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,6 +27,22 @@ public class UserController {
     public void addUser(@RequestBody User user){
         userService.addUser(user);
     }
+
+    @DeleteMapping(path = "{userid}")
+    public void deleteUser(@PathVariable("userid") Integer userid){
+        userService.deleteUser(userid);
+    }
+
+    @PutMapping(path = "{userid}")
+    public void updateUser(@PathVariable("userid") Integer userid,
+                           @RequestParam(required = false) String TenHienThi,
+                           @RequestParam(required = false) String DiaChi,
+                           @RequestParam(required = false) String GioiTinh,
+                           @RequestParam(required = false) String SDT) {
+        userService.updateUser(userid ,TenHienThi, DiaChi, GioiTinh, SDT);
+
+    }
+
 
 
 }
--- backend/backend/src/main/java/com/foodapp/backend/users/UserService.java
+++ backend/backend/src/main/java/com/foodapp/backend/users/UserService.java
@@ -2,8 +2,10 @@ package com.foodapp.backend.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserService {
@@ -34,6 +36,47 @@ public class UserService {
 
     public void addUser(User user) {
         System.out.println(user);
+        userRepository.save(user);
+
+
+
+    }
+
+    public void deleteUser(Integer userid) {
+       boolean isUserExist = userRepository.existsById(userid);
+       if(!isUserExist){
+           throw new IllegalStateException("User with id: "+ userid + " doesnt exist" );
+       }
+       userRepository.deleteById(userid);
+    }
+
+
+    @Transactional
+    public void updateUser(Integer userid,String tenHienThi, String diaChi, String gioiTinh, String sdt) {
+        User user = userRepository.findById(userid).orElseThrow(() -> new IllegalStateException("user with id: "+ userid + " doesnt exist"));
+
+        if(tenHienThi !=null && tenHienThi.length()> 0 && !Objects.equals(user.getTenHienThi(), tenHienThi)){
+            user.setTenHienThi(tenHienThi);
+
+        }
+        // Update DiaChi
+        if (diaChi != null && !diaChi.isBlank()
+                && !Objects.equals(user.getDiaChi(), diaChi)) {
+            user.setDiaChi(diaChi);
+        }
+
+        // Update GioiTinh
+        if (gioiTinh != null && !gioiTinh.isBlank()
+                && !Objects.equals(user.getGioiTinh(), gioiTinh)) {
+            user.setGioiTinh(gioiTinh);
+        }
+
+        // Update SDT
+        if (sdt != null && !sdt.isBlank()
+                && !Objects.equals(user.getSDT(), sdt)) {
+            user.setSDT(sdt);
+        }
+
 
     }
 }
