f43d442e29e048032cdd8990af0af471b0db4579
Nguyễn Hoàng Gia Bảo <nhgbao12345@gmail.com>
2025-12-10 19:59:51 +0700

    Update dialogForgot

--- Android/app/src/main/java/com/example/ttcn_dangnhap/forgot.java
+++ Android/app/src/main/java/com/example/ttcn_dangnhap/forgot.java
@@ -2,6 +2,8 @@ package com.example.ttcn_dangnhap;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -9,6 +11,7 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -31,7 +34,7 @@ import java.util.Map;
 public class forgot extends AppCompatActivity {
     TextView txt_back;
     Button btn_gui;
-    EditText txt_sdt_recover;
+    EditText txt_email_recover;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,25 +52,25 @@ public class forgot extends AppCompatActivity {
     private void addEvents() {
         txt_back.setOnClickListener(view -> finish());
         btn_gui.setOnClickListener(view -> {
-            String sdt=txt_sdt_recover.getText().toString();
-            if (sdt.isEmpty())
+            String email=txt_email_recover.getText().toString();
+            if (email.isEmpty())
             {
-                txt_sdt_recover.setError("Vui lòng nhập số điện thoại!");
+                txt_email_recover.setError("Vui lòng nhập số email !");
                 return;
             }
             else
             {
-                resetMk(sdt);
+                resetMk(email);
             }
         });
     }
 
     private void addControls() {
         txt_back= findViewById(R.id.txt_back);
-        txt_sdt_recover=findViewById(R.id.txt_sdt_recover);
+        txt_email_recover=findViewById(R.id.txt_email_recover);
         btn_gui=findViewById(R.id.btn_gui);
     }
-    private void resetMk(String sdt)
+    private void resetMk(String email)
     {
         String url = "http://10.0.2.2:8080/forgot";
         RequestQueue queue = Volley.newRequestQueue(this);
@@ -81,6 +84,7 @@ public class forgot extends AppCompatActivity {
                             String message = jsonObject.getString("message");
                             if (status.equals("success")) {
                                 ThongBao.showThongBao(forgot.this,"Thông báo",message,null);
+                                hienThiDialogNhapMa(email);
                             } else {
                                 ThongBao.showThongBao(forgot.this,"Thất bại",message,null);
                             }
@@ -101,10 +105,31 @@ public class forgot extends AppCompatActivity {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> params = new HashMap<>();
-                params.put("sdt",sdt);
+                params.put("email",email);
                 return params;
             }
         };
         queue.add(postRequest);
+    }
+    private void hienThiDialogNhapMa(String email)
+    {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogview = inflater.inflate(R.layout.layout_authencode,null);
+        dialog.setView(dialogview);
+        AlertDialog alert = dialog.create();
+        EditText txtCodeAuthen = dialogview.findViewById(R.id.txt_code_authen);
+        Button btnAuthen = dialogview.findViewById(R.id.btn_authen);
+        btnAuthen.setOnClickListener(view -> {
+            String code = txtCodeAuthen.getText().toString();
+            if (code.isEmpty())
+            {
+                txtCodeAuthen.setError("Vui lòng nhập mã xác thực !");
+            }
+            else
+            {
+               // xacThucMaCode(email, code, dialog);
+            }
+        });
     }
 }
