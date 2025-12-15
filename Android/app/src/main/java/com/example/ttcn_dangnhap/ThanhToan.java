ef936dd28f0557de2673f5a6122c9503e28779ba
Nguyễn Hoàng Gia Bảo <nhgbao12345@gmail.com>
2025-12-15 15:22:31 +0700

    .

--- Android/app/src/main/java/com/example/ttcn_dangnhap/ThanhToan.java
+++ Android/app/src/main/java/com/example/ttcn_dangnhap/ThanhToan.java
@@ -2,14 +2,27 @@ package com.example.ttcn_dangnhap;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ttcn_dangnhap.adapter.CustomCartListAdapter;
 
@@ -19,9 +32,13 @@ import models.Cart.CartItem;
 
 public class ThanhToan extends AppCompatActivity {
     TextView tvTongTienThanhToan;
+    ImageView imgBack;
     ListView lvDanhSachMonThanhToan;
     List<CartItem> listThanhToan;
-    com.example.ttcn_dangnhap.adapter.CustomCartListAdapter adapter;
+    Button btnThanhtoan,btnSua;
+    TextView tvUserName, tvUserPhone, tvAddress;
+    RadioButton rbCOD, rbVNPay;
+    CustomCartListAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,28 +51,99 @@ public class ThanhToan extends AppCompatActivity {
         });
         addControls();
         getDataFromIntent();
+        addEvents();
+    }
+
+    private void addEvents() {
+        btnSua.setOnClickListener(view -> {showEdit();});
+        imgBack.setOnClickListener(view -> {finish();});
+        btnThanhtoan.setOnClickListener(view -> {
+            if (!rbCOD.isChecked() && !rbVNPay.isChecked()) {
+                Toast.makeText(this, "Vui lòng chọn phương thức thanh toán!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            //long totalAmount = parseAmount(tvTongTienThanhToan.getText().toString());
+            if (rbCOD.isChecked())
+            {
+
+            }
+        });
     }
 
     private void addControls() {
-        tvTongTienThanhToan = findViewById(R.id.tvTotalAmount); // Ví dụ ID
+        tvTongTienThanhToan = findViewById(R.id.tvTotalAmount);
         lvDanhSachMonThanhToan = findViewById(R.id.rvCartItems);
+        btnSua=findViewById(R.id.btnSua);
+        btnThanhtoan=findViewById(R.id.btnThanhtoan);
+        tvUserName=findViewById(R.id.tvUserName);
+        tvUserPhone=findViewById(R.id.tvUserPhone);
+        tvAddress=findViewById(R.id.tvAddress);
+        rbCOD=findViewById(R.id.rbCOD);
+        rbVNPay=findViewById(R.id.rbVNPay);
+        imgBack=findViewById(R.id.btnBack);
     }
     void getDataFromIntent() {
         Intent intent = getIntent();
 
-        // 1. Nhận tổng tiền
         String tongTien = intent.getStringExtra("TOTAL_PRICE");
         if(tongTien != null) {
             tvTongTienThanhToan.setText(tongTien + "đ");
         }
 
-        // 2. Nhận danh sách món ăn
-        // Cần ép kiểu về List<CartItem>
+
         listThanhToan = (List<CartItem>) intent.getSerializableExtra("CART_LIST");
 
         if (listThanhToan != null && listThanhToan.size() > 0) {
             adapter = new CustomCartListAdapter(this, listThanhToan, null);
             lvDanhSachMonThanhToan.setAdapter(adapter);
+            setListViewHeight(lvDanhSachMonThanhToan);
         }
+    }
+    private void showEdit()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.layout_sua_tt_nhan, null);
+        builder.setView(dialogView);
+        AlertDialog dialog = builder.create();
+        EditText edtName = dialogView.findViewById(R.id.edtName);
+        EditText edtPhone = dialogView.findViewById(R.id.edtPhone);
+        EditText edtAddress = dialogView.findViewById(R.id.edtAddress);
+        AppCompatButton btnConfirm = dialogView.findViewById(R.id.btnConfirm);
+        edtName.setText(tvUserName.getText().toString());
+        edtPhone.setText(tvUserPhone.getText().toString());
+        edtAddress.setText(tvAddress.getText().toString());
+        btnConfirm.setOnClickListener(view -> {
+            String newName = edtName.getText().toString().trim();
+            String newPhone = edtPhone.getText().toString().trim();
+            String newAddress = edtAddress.getText().toString().trim();
+            if (newName.isEmpty() || newPhone.isEmpty() || newAddress.isEmpty()) {
+                Toast.makeText(ThanhToan.this, "Vui lòng nhập đủ thông tin", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            tvUserName.setText(newName);
+            tvUserPhone.setText(newPhone);
+            tvAddress.setText(newAddress);
+            dialog.dismiss();
+
+            Toast.makeText(ThanhToan.this, "Đã cập nhật thông tin", Toast.LENGTH_SHORT).show();
+        });
+        dialog.show();
+    }
+    void setListViewHeight(ListView lsView){
+        ListAdapter adapter = lsView.getAdapter();
+        if(adapter == null) return;
+        int totalHeight = 0;
+        for(int i = 0 ; i< adapter.getCount(); i++){
+
+            View listItem = adapter.getView(i, null, lsView);
+            listItem.measure(0,0);
+            totalHeight+=listItem.getMeasuredHeight();
+
+        }
+        ViewGroup.LayoutParams params = lsView.getLayoutParams();
+        params.height = totalHeight + (lsView.getDividerHeight() * (adapter.getCount()-1));
+        lsView.setLayoutParams(params);
+        lsView.requestLayout();
     }
 }
